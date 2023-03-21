8ecb750d159952ff6d2b8a5c820d4888e9bef273
chijioke <ejiofor.valentino@gmail.com>
2023-03-20 17:54:14 -0700

    c commit 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry Point
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 } 
