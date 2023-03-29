48dd4c4f5c063e78f19cc5d9be7adcde330ea701
chijioke <ejiofor.valentino@gmail.com>
2023-03-28 19:40:35 -0700

    commit hello 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: aways 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 25-10-19\n", stdout);
+	return (1);
+}
