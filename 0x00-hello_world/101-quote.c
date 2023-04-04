51ee04c8794d848fd33ab0919cead4e8310014d6
chijioke <ejiofor.valentino@gmail.com>
2023-04-04 11:51:00 -0700

    commit correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 25-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 25-10-19\n", stdout);
 	return (1);
 }
